a074acaabfe481eeabb3a39f48a29c1397d7cf4d
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-04-29 18:26:28 -0300

    Add UI

---
+++ Assets/Scripts/BeanSpawner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BeanSpawner : MonoBehaviour
+{
+    public GameObject beanPrefab; // Assign your bean prefab in the inspector
+    public Vector3 centerPosition; // Center position of the spawning area
+    public int numberOfBeans = 200; // Number of beans to spawn
+    public float spawnAreaSize = 0.05f; // Size of the spawn area in meters (5 cm)
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfBeans; i++)
+        {
+            SpawnBean();
+        }
+    }
+
+    void SpawnBean()
+    {
+        Vector3 spawnPosition = centerPosition + new Vector3(
+            (Random.value - 0.5f) * spawnAreaSize,
+            (Random.value - 0.5f) * spawnAreaSize,
+            (Random.value - 0.5f) * spawnAreaSize
+        );
+
+        Instantiate(beanPrefab, spawnPosition, Quaternion.identity);
+    }
+}
